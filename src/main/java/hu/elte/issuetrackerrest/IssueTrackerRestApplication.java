c174c693b9c04a5e72727f98e88978bd72986d5d
Győző Horváth <gyozo.horvath@inf.elte.hu>
2018-09-21 09:40:27 +0200

    Spring initializer generation

---
+++ src/main/java/hu/elte/issuetrackerrest/IssueTrackerRestApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.issuetrackerrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackerRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrackerRestApplication.class, args);
+	}
+}
