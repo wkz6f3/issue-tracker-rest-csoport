4ff1d52e816bf18122f8732c2397a02d033b93ef
Győző Horváth <gyozo.horvath@inf.elte.hu>
2018-09-21 10:26:34 +0200

    Adding default data to database (data.sql)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into issue (title, description, place, created_at, updated_at) values ('issue1', 'description1', 'place1', CURRENT_TIMESTAMP(),  CURRENT_TIMESTAMP());
+insert into issue (title, description, place, created_at, updated_at) values ('issue2', 'description2', 'place2', CURRENT_TIMESTAMP(),  CURRENT_TIMESTAMP());
+insert into issue (title, description, place, created_at, updated_at) values ('issue3', 'description3', 'place3', CURRENT_TIMESTAMP(),  CURRENT_TIMESTAMP());
+insert into issue (title, description, place, created_at, updated_at) values ('issue4', 'description4', 'place4', CURRENT_TIMESTAMP(),  CURRENT_TIMESTAMP());
