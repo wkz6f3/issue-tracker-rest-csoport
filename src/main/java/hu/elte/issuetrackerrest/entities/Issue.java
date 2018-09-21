61e8dbfc7642278f23611e5b8a552073ae12b404
Győző Horváth <gyozo.horvath@inf.elte.hu>
2018-09-21 10:20:13 +0200

    Adding Issue entity

---
+++ src/main/java/hu/elte/issuetrackerrest/entities/Issue.java
@@ -0,0 +1,43 @@
+package hu.elte.issuetrackerrest.entities;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Issue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column
+    private String place;
+
+    @Column
+    @CreationTimestamp
+    private LocalDateTime created_at;
+
+    @Column
+    @UpdateTimestamp
+    private LocalDateTime updated_at;
+}
